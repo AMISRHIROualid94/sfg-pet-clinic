3e5c89eaa84567b2436a0d19bec714fb9113e263
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2022-08-11 17:01:16 +0100

    last Commit

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.Test;
+
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
